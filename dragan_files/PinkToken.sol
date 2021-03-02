85d513f1bd311d0676f7c589282acd1781d3b6f8
Dragan Bogatic <dbogaticE@hotmail.com>
2021-03-02 13:35:10 -0600

    upload dragan working files

---
+++ dragan_files/PinkToken.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.5.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/drafts/Counters.sol";
+
+contract Memorabillia is ERC721Full {
+
+    constructor() ERC721Full("PinkToken", "PNKT") public { }
+
+    using Counters for Counters.Counter;
+    Counters.Counter token_ids;
+
+    struct ColorToken {
+        string name;
+        string description;
+
+    }
+
+    mapping(uint => ColorToken) public memorabillia_collection;
+
+    event tokenOrder(uint token_id, uint value, string order_uri);
+
+    function registerColorToken(address owner, string memory name, string memory description, string memory token_uri) public returns(uint) {
+        token_ids.increment();
+        uint token_id = token_ids.current();
+
+        _mint(owner, token_id);
+        _setTokenURI(token_id, token_uri);
+
+        memorabillia_collection[token_id] = ColorToken(name, description);
+
+        return token_id;
+
+        
+    }
+
+}

09620e69cd3a32e51482aa615a12b496244e5d86
Dragan Bogatic <dbogaticE@hotmail.com>
2021-03-02 15:52:25 -0600

    pink token edit

--- dragan_files/PinkToken.sol
+++ dragan_files/PinkToken.sol
@@ -5,10 +5,15 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 
 contract Memorabillia is ERC721Full {
 
-    constructor() ERC721Full("PinkToken", "PNKT") public { }
-
+    constructor() ERC721Full("PinkToken", "PNKT") public {
+        
+    }
+    
+    uint price = 1000000000000000000;
+    
+    
     using Counters for Counters.Counter;
-    Counters.Counter token_ids;
+        Counters.Counter token_ids;
 
     struct ColorToken {
         string name;
@@ -19,20 +24,18 @@ contract Memorabillia is ERC721Full {
 
     mapping(uint => ColorToken) public memorabillia_collection;
 
-    event tokenOrder(uint token_id, uint value, string order_uri);
+    event tokenOrder(uint token_id, uint price, string order_uri);
 
-    function orderPrint(address buyer, string memory name, string memory description, uint price, string memory token_uri) public payable returns(uint) {
+    function orderPrint(address buyer, string memory name, string memory description, uint price_paid, string memory token_uri) public payable returns(uint) {
         token_ids.increment();
         uint token_id = token_ids.current();
 
         _mint(buyer, token_id);
         _setTokenURI(token_id, token_uri);
-
-        memorabillia_collection[token_id] = ColorToken(name, description, price);
+      
+        memorabillia_collection[token_id] = ColorToken(name, description, price_paid);
 
         return token_id;
-
         
     }
-
 }

50c02fcc4db5141b26e47569651ab624a8de2ee2
Dragan Bogatic <dbogaticE@hotmail.com>
2021-03-04 19:56:14 -0600

    update sol and upload json

--- dragan_files/BlueToken.sol
+++ dragan_files/BlueToken.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.5.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/drafts/Counters.sol";
 
-contract PinkCoin is ERC721Full {
+contract BlueCoin is ERC721Full {
 
-    constructor() ERC721Full("PinkToken", "PINKC") public {
+    constructor() ERC721Full("BlueToken", "BLUEC") public {
         
     }
     

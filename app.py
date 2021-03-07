93bbe3a3b9de785f8cb045b15924d1dfaf83f67a
jowens520 <jonathan.owens@gmail.com>
2021-03-06 20:36:13 -0600

    Added contractReceipt request to order function in app.py.

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ def home():
 # Capture and process order
 @app.route("/order")
 def order(self, args):
-    pass
+    color = request.args.get('contractReceipt')
+    return f'<h1>Contract Receipt Information: {color}</h1>'
+
 
 # Capture and process blue token order
 # test url

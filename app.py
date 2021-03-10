6b1fa6c604677e6a388493dfa06557ed76cf2509
jowens520 <jonathan.owens@gmail.com>
2021-03-09 21:40:52 -0600

    Added for loops for each token color and added pull_order_information function to app.py.

--- app.py
+++ app.py
@@ -32,12 +32,12 @@ def gfg():
     # Push data to contract goes here
     contract_response = contract_api.push_to_contract(first_name, last_name)
 
-    # Pull new order information from contract
-    token_ids = contract_api.pull_token_ids()
-
-    # loop here over new_order_data to process each order using token_id
-    for token_id in token_ids:
-        if coin_color == 'blue':
+    if coin_color == 'blue':
+        # Pull new order information from contract
+        token_ids = contract_api.pull_token_ids('blue')
+        
+        # loop here over new_order_data to process each order using token_id
+        for token_id in token_ids:
             # Call blue contract using passed token id for order information
             order_info = contract_api.pull_order_information(token_id, 'blue')
             country = order_info[1]
@@ -54,7 +54,12 @@ def gfg():
             
             order_response = printing_api.submit_order(access_token, material_id, model_id, 'crypto', 'Cache', country, state, city, address1, ' ', zip_code, ' ')
 
-        if coin_color == 'pink':    
+    if coin_color == 'pink':    
+        # Pull new order information from contract
+        token_ids = contract_api.pull_token_ids('pink')
+        
+        # loop here over new_order_data to process each order using token_id
+        for token_id in token_ids:
             # Call blue contract using passed token id for order information
             order_info = contract_api.pull_order_information(token_id, 'pink')
             country = order_info[1]
@@ -71,8 +76,8 @@ def gfg():
             
             order_response = printing_api.submit_order(access_token, material_id, model_id, 'crypto', 'Cache', country, state, city, address1, ' ', zip_code, ' ')
         
-        if coin_color == 'white':
-            order_response = 'Error: No color token passed.'
+    if coin_color == 'white':
+        order_response = 'Error: No color token passed.'
 
     return f'<h1>Contract response: {contract_response}.  Status response for {coin_color} token: {order_response}</h1>'
 

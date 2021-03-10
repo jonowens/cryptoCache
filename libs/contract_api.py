bb499877aa4e770f50a248161926389279e45a2a
jowens520 <jonathan.owens@gmail.com>
2021-03-09 21:30:46 -0600

    Added scope code to pull_token_ids function in contract_api.py.

--- libs/contract_api.py
+++ libs/contract_api.py
@@ -5,15 +5,50 @@ from dotenv import load_dotenv
 from web3 import Web3
 import os
 import json
+import pandas as pd
 
 def push_to_contract(first_name, last_name):
     first_name = first_name
     last_name = last_name
     return "This is a place holder.  It works."
 
-def pull_token_ids():
+def pull_token_ids(color_token):
+
+    # Instantiate variable
+    not_processed_token_ids = []
+    pink_contract_address = '0x3607844eb2eC711279B2F1831E1784Bc7423713f'
+    blue_contract_address = '0xC86C8d3E16370dd76e73DdeEf15767E343331816'
     
-    return "This is new token ids.  It works."
+    project_key = os.getenv('INFURA_PROJECT_ID')
+    w3 = Web3(Web3.HTTPProvider(f"https://kovan.infura.io/v3/{project_key}"))
+    
+    load_dotenv()
+
+    if color_token == 'blue':
+        with open("../static/blueContract.json") as f:
+            info_json = json.load(f)
+        abi = info_json["abi"]
+        contract = w3.eth.contract(blue_contract_address, abi = abi)
+    
+    elif color_token == 'pink':
+        with open("../static/pinkContract.json") as f:
+            info_json = json.load(f)
+        abi = info_json["abi"]
+        contract = w3.eth.contract(pink_contract_address, abi = abi)
+    
+
+    order_filter = contract.events.tokenOrder.createFilter(fromBlock='earliest')
+    all_orders = order_filter.get_all_entries()
+    data = pd.read_csv('../data/order.csv')
+
+
+    for order in all_orders[data['last_token_id'][0] : ]:
+        not_processed_token_ids.append(order.args['tokenId'])
+    data['last_token_id'][0] = not_processed_token_ids[-1]
+
+    data.to_csv('../data/order.csv', index = False)
+
+    return not_processed_token_ids
 
 def pull_order_information(token_id, coin_color):
     # Instantiate variables

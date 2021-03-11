cbe5c7a28440c16a55009772dd229cc70b3347c6
jowens520 <jonathan.owens@gmail.com>
2021-03-10 22:20:55 -0600

    Removed push_to_contract function from contract_api.py.

--- libs/contract_api.py
+++ libs/contract_api.py
@@ -7,34 +7,6 @@ import os
 import json
 import pandas as pd
 
-def push_to_contract(color_token, buyer_address, city, state, zip, street, country):
-
-    # Code to instantiate contract goes here
-    pink_contract_address = '0x541b0A62B9Ff59370683EE40d22eaEB4eB231578'
-    blue_contract_address = '0x2E9974EC97D17181aFEE5e1e3F3CfD3237C7a079'
-    
-    project_key = os.getenv('INFURA_PROJECT_ID')
-    w3 = Web3(Web3.HTTPProvider(f"https://kovan.infura.io/v3/{project_key}"))
-    
-    load_dotenv()
-
-    if color_token == 'blue':
-        with open("../static/blueContract.json") as f:
-            info_json = json.load(f)
-        abi = info_json["abi"]
-        contract = w3.eth.contract(blue_contract_address, abi = abi)
-    
-    elif color_token == 'pink':
-        with open("../static/pinkContract.json") as f:
-            info_json = json.load(f)
-        abi = info_json["abi"]
-        contract = w3.eth.contract(pink_contract_address, abi = abi)
-
-    # Code to communicate with orderPrint(buyer_address, country, state, city, street, zip) goes here
-    post_response = contract.functions.orderPrint(buyer_address, country, state, city, street, zip)
-
-    return post_response
-
 def pull_token_ids(color_token):
 
     # Instantiate variable
@@ -48,13 +20,13 @@ def pull_token_ids(color_token):
     load_dotenv()
 
     if color_token == 'blue':
-        with open("../static/blueContract.json") as f:
+        with open("./static/blueContract.json") as f:
             info_json = json.load(f)
         abi = info_json["abi"]
         contract = w3.eth.contract(blue_contract_address, abi = abi)
     
     elif color_token == 'pink':
-        with open("../static/pinkContract.json") as f:
+        with open("./static/pinkContract.json") as f:
             info_json = json.load(f)
         abi = info_json["abi"]
         contract = w3.eth.contract(pink_contract_address, abi = abi)
@@ -69,7 +41,7 @@ def pull_token_ids(color_token):
         not_processed_token_ids.append(order.args['tokenId'])
     data['last_token_id'][0] = not_processed_token_ids[-1]
 
-    data.to_csv('../data/order.csv', index = False)
+    data.to_csv('./data/order.csv', index = False)
 
     return not_processed_token_ids
 
@@ -85,7 +57,7 @@ def pull_order_information(token_id, coin_color):
     if coin_color == 'pink':
         w3 = Web3(Web3.HTTPProvider(f"https://kovan.infura.io/v3/{project_key}"))
 
-        with open("../static/pinkContract.json") as f:
+        with open("./static/pinkContract.json") as f:
             info_json = json.load(f)
         abi = info_json["abi"]
         
